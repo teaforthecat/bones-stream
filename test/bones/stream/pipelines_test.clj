af6b1fa987e8720ac8425357fc2e17be438994dc
Chris Thompson <teaforthecat@gmail.com>
2017-07-10 22:12:09 -0500

    [test] try running against services

--- test/bones/stream/pipelines_test.clj
+++ test/bones/stream/pipelines_test.clj
@@ -6,38 +6,38 @@
             [com.stuartsierra.component :as component]
             [bones.stream.protocols :as p]
             [bones.stream.jobs :as jobs]))
-(comment ;WIP
-
-  (deftest pipeline-builders
-    (testing "builds a valid pipeline from a job"
-      (let [job (jobs/series-> {}
-                               (jobs/input :kafka)
-                               (jobs/output :redis))
-            pipe (component/start (pipelines/pipeline job))]
-        (are [path value] (= value (get-in pipe path))
-          [:input :task-map :onyx/medium] :kafka
-          [:output :task-map :onyx/params] [:redis/spec :redis/channel])))
-
-    (testing "starts the input and outputs"
-      (let  [topic "test123"
-             job (jobs/series-> {}
-                                (jobs/input :kafka)
-                                (jobs/output :redis {:redis/channel topic}))
-             ;; this will need to go somewhere...
-             _ (jobs/serialization-format :msgpack)
-             pipe (component/start (pipelines/pipeline job))
-             stream (ms/stream)]
 
 
-        ;; the peers aren't running so nothing will arrive at the output.
-        ;; (ms/consume println (p/output pipe stream)) ;; doesn't blow up is good
-        (p/output pipe stream) ;; doesn't blow up is good
+(deftest pipeline-builders
+  (testing "builds a valid pipeline from a job"
+    (let [job (jobs/series-> {}
+                             (jobs/input :kafka)
+                             (jobs/output :redis))
+          pipe (component/start (pipelines/pipeline job))]
+      (are [path value] (= value (get-in pipe path))
+        [:input :task-map :onyx/medium] :kafka
+        [:output :task-map :onyx/params] [:redis/spec :redis/channel])))
 
-        ;; fake the other side of the pipeline here because the peers aren't running
-        (p/publish (:redi (:output pipe)) topic {:abc 123})
+  (testing "starts the input and outputs"
+    (let  [topic "test123"
+           job (jobs/series-> {}
+                              (jobs/input :kafka)
+                              (jobs/output :redis {:redis/channel topic}))
+           ;; this will need to go somewhere...
+           _ (jobs/serialization-format :msgpack)
+           pipe (component/start (pipelines/pipeline job))
+           stream (ms/stream)]
+
+
+      ;; the peers aren't running so nothing will arrive at the output.
+      ;; (ms/consume println (p/output pipe stream)) ;; doesn't blow up is good
+      (p/output pipe stream) ;; doesn't blow up is good
+
+      ;; fake the other side of the pipeline here because the peers aren't running
+      (p/publish (:redi (:output pipe)) topic {:abc 123})
+      ;; a valid response
+      (= {:abc 123} @(ms/take! stream))
+      (let [response (p/input pipe {:key 123 :value "hi" :topic topic})]
         ;; a valid response
-        (= {:abc 123} @(ms/take! stream))
-        (let [response (p/input pipe {:key 123 :value "hi" :topic topic})]
-          ;; a valid response
-          (is (= '(:topic :partition :offset) (keys response)))))
-      )))
+        (is (= '(:topic :partition :offset) (keys response)))))
+    ))
